35b91c42359d6dbcbe7b7eee6a1172832a91dfe8
gjyli <gjylimekaj1@gmail.com>
2024-09-08 12:54:48 +0200

    added some new methods in test1 and test2.js

--- cypress/integration/examples/Test2.js
+++ cypress/integration/examples/Test2.js
@@ -1,16 +1,10 @@
 /// <reference types="cypress" />
 
-describe('My first test suite', function(){
-    it('My first test case', function(){
+describe('My second test suite', function(){
+    it('My second test case', function(){
         cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
         cy.get('.search-keyword').type('ca')
-        cy.wait(2000)
-        cy.get('.product:visible').should('have.length',4)
         cy.get('.products').as('productLocator')
-        cy.get('@productLocator').find('.product').should('have.length',4)
-        cy.get('@productLocator').find('.product').eq(2).contains('ADD TO CART').click().then(function(){
-            console.log('hello wolrd!!!!')
-        })
         //$el: The current element being processed in the collection. This is a wrapped jQuery object, so you can use jQuery methods on it.
         //index: The index of the current element in the collection, starting from 0.
         //$list: The entire list of elements as a jQuery object array.
@@ -21,12 +15,8 @@ describe('My first test suite', function(){
                 cy.wrap($el).find('button').click()
             }
         })
-        //assert if logo text is correctly displayed
-        cy.get('.brand').should('have.text','GREENKART')
-        //this is to print in logs
-        cy.get('.brand').then(function(logoelement){
-            cy.log(logoelement.text())
-        })
-        //cy.log(logo.text())
+        cy.get('.cart-icon').click()
+        cy.contains('PROCEED TO CHECKOUT').click()
+        cy.contains('Place Order').click()
     })
 })
